d6eb77de9063e8063f54a946bec0cf7c8866f8b8
srujan_kumar92 <srujanthegoldenboy@gmail.com>
2018-08-17 15:30:16 +0530

    Removed slot

--- src/main/java/com/witcurve/repository/SlotRepository.java
+++
@@ -1,9 +0,0 @@
-package com.witcurve.repository;
-
-import com.witcurve.domain.Slot;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SlotRepository extends JpaRepository<Slot, Long> {
-}
