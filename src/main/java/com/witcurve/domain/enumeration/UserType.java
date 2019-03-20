1be85c7d8db406057ce40dd548a2b0682f022686
anuranjankumar <iit.anuranjan@gmail.com>
2019-03-20 13:39:30 +0530

    few changes as per discussion

--- src/main/java/com/witcurve/domain/enumeration/UserType.java
+++ src/main/java/com/witcurve/domain/enumeration/UserType.java
@@ -2,11 +2,10 @@ package com.witcurve.domain.enumeration;
 
 public enum UserType {
     SUPER_USER, //Only one user per db who has ultimate powerrrrrr.
-    INSTITUTE_USER, // to be assigned to only one user per institute: with authority InstituteAdmin
-    SCHOOL_USER,
-    SCHOOL_INFO_USER,
+    INSTITUTE_MANAGER, // to be assigned to only one user per institute: with authority InstituteAdmin
+    SCHOOL_MANAGER,
+    SCHOOL_BOARD_MANAGER,
     TEACHING_STAFF,
     NON_TEACHING_STAFF,
-    GROUND_STAFF,
     PARENT
 }
