74021ef4a38bd7099694c2a7642adf60325c24ed
anuranjankumar <iit.anuranjan@gmail.com>
2019-04-11 22:42:45 +0530

    added active condition for getting standard list

--- src/main/java/com/witcurve/repository/StandardRepository.java
+++ src/main/java/com/witcurve/repository/StandardRepository.java
@@ -13,15 +13,15 @@ public interface StandardRepository extends JpaRepository<Standard,Long> {
 
     Standard findByClassTeacherId(Long classTeacherId);
 
-    @Query("select std from Standard std where std.schoolInfo.id = ?1 order by std.grade, std.section")
+    @Query("select std from Standard std where std.schoolInfo.id = ?1 and std.active=true order by std.grade, std.section")
     List<Standard> findBySchoolInfoId(Long schoolInfoId);
 
-    @Query("select std from Standard std where std.grade=?1 and std.section=?2 and std.schoolInfo.id = ?3")
+    @Query("select std from Standard std where std.grade=?1 and std.section=?2 and std.schoolInfo.id = ?3 and std.active=true")
     Standard findByGradeAndSectionAndSchoolInfoId(Grade grade, String section, Long schoolInfoId);
 
-    @Query("select std from Standard std where std.schoolInfo.id = ?1 and std.id in (select gsd.standard.id from GeneralSlotDetails gsd where gsd.status='ACTIVE' and gsd.standard.id=std.id) order by std.grade, std.section")
+    @Query("select std from Standard std where std.schoolInfo.id = ?1 and std.active=true and std.id in (select gsd.standard.id from GeneralSlotDetails gsd where gsd.status='ACTIVE' and gsd.standard.id=std.id) order by std.grade, std.section")
     List<Standard> findSlotAssignedBySchoolInfoId(Long schoolInfoId);
 
-    @Query("select std from Standard std where std.schoolInfo.id = ?1 and std.id not in (select gsd.standard.id from GeneralSlotDetails gsd where gsd.status='ACTIVE' and gsd.standard.id=std.id)  order by std.grade, std.section")
+    @Query("select std from Standard std where std.schoolInfo.id = ?1 and std.active=true and std.id not in (select gsd.standard.id from GeneralSlotDetails gsd where gsd.status='ACTIVE' and gsd.standard.id=std.id)  order by std.grade, std.section")
     List<Standard> findSlotUnassignedBySchoolInfoId(Long schoolInfoId);
 }
