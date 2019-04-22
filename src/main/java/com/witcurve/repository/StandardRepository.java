4c221712993278f2b861b605fcb003688151601c
anuranjankumar <iit.anuranjan@gmail.com>
2019-04-22 23:07:09 +0530

    standard creation fix

--- src/main/java/com/witcurve/repository/StandardRepository.java
+++ src/main/java/com/witcurve/repository/StandardRepository.java
@@ -17,7 +17,7 @@ public interface StandardRepository extends JpaRepository<Standard,Long> {
     @Query("select std from Standard std where std.schoolInfo.id = ?1 and std.active = true order by std.grade, std.section")
     List<Standard> findBySchoolInfoId(Long schoolInfoId);
 
-    @Query("select std from Standard std where std.grade=?1 and std.section=?2 and std.schoolInfo.id = ?3 and std.active=true")
+    @Query("select std from Standard std where std.grade=?1 and std.section=?2 and std.schoolInfo.id = ?3")
     Standard findByGradeAndSectionAndSchoolInfoId(Grade grade, String section, Long schoolInfoId);
 
     @Query("select std from Standard std where std.schoolInfo.id = ?1 and std.active=true and std.id in (select gsd.standard.id from GeneralSlotDetails gsd where gsd.status='ACTIVE' and gsd.standard.id=std.id) order by std.grade, std.section")
