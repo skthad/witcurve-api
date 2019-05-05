5555ee583b4f89481e0b3d7a161fff886b8b2951
srujan_kumar92 <srujanthegoldenboy@gmail.com>
2019-05-05 18:51:17 +0530

    getting exam by status

--- src/main/java/com/witcurve/repository/ExamRepository.java
+++ src/main/java/com/witcurve/repository/ExamRepository.java
@@ -14,6 +14,14 @@ import java.util.List;
 public interface ExamRepository extends JpaRepository<Exam, Long> {
 
     @Query("select e from Exam e where e.schoolInfo.id = ?1 " +
+        "and e.startDate between ?2 and ?3 and e.status in ?4 order by e.startDate desc")
+    List<Exam> findAllBySchoolInfoAndDateRangeAndStatuses(Long schoolInfoId, LocalDate startDate, LocalDate endDate, List<ExamStatus> statusList);
+
+    @Query("select distinct gsd.exam from GeneralSlotDetails gsd where gsd.exam.schoolInfo.id = ?1 and gsd.grade=?2 " +
+        "and gsd.exam.startDate between ?3 and ?4 and gsd.exam.status in ?5 order by gsd.exam.startDate desc")
+    List<Exam> findAllBySchoolInfoAndGradeAndDateRangeAndStatuses(Long schoolInfoId, Grade grade, LocalDate startDate, LocalDate endDate, List<ExamStatus> statusList);
+
+    @Query("select e from Exam e where e.schoolInfo.id = ?1 " +
         "and e.startDate between ?2 and ?3 order by e.startDate desc")
     List<Exam> findAllBySchoolInfoAndDateRange(Long schoolInfoId, LocalDate startDate, LocalDate endDate);
 
