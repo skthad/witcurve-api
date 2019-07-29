c020cace3bd8435092f1e1f6b87d293e772dee78
Vishal Gidwani <vishal.gidwani@beehyv.com>
2019-07-29 22:50:56 +0530

    added scheduled method to clear transactions stuck in pending status

--- src/main/java/com/witcurve/repository/PaymentOrderRepository.java
+++ src/main/java/com/witcurve/repository/PaymentOrderRepository.java
@@ -1,9 +1,14 @@
 package com.witcurve.repository;
 
 import com.witcurve.domain.PaymentOrder;
+import com.witcurve.domain.enumeration.TransactionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaymentOrderRepository extends JpaRepository<PaymentOrder, Long> {
+
     PaymentOrder findByOrderId(String orderId);
-    PaymentOrder findByStudentIdAndOrderId(Long studentId, Long orderId);
+
+    List<PaymentOrder> findByTransactionStatus(TransactionStatus transactionStatus);
 }
