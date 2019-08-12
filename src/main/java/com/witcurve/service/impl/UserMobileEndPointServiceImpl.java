0281ebd3ba3d5dad161e090f57b8df1cd4ed98e2
anuranjankumar <iit.anuranjan@gmail.com>
2019-08-12 19:18:31 +0530

    staff eligibility order

--- src/main/java/com/witcurve/service/impl/UserMobileEndPointServiceImpl.java
+++ src/main/java/com/witcurve/service/impl/UserMobileEndPointServiceImpl.java
@@ -45,7 +45,7 @@ public class UserMobileEndPointServiceImpl implements UserMobileEndPointService 
         log.debug("Add user mobile end point : {}", userMobileEndPointDTO);
         UserMobileEndPoint existingUserMobileEndPoint = userMobileEndPointRepository.findByUserIdAndToken(userMobileEndPointDTO.getUserId(), userMobileEndPointDTO.getDeviceToken());
         if(existingUserMobileEndPoint != null) {
-            return userMobileEndPointMapper.toDto(existingUserMobileEndPoint);
+            userMobileEndPointDTO = userMobileEndPointMapper.toDto(existingUserMobileEndPoint);
         }
         Long schoolInfoId = schoolInfoService.getSchoolInfoIdByUserId(userMobileEndPointDTO.getUserId());
         String schoolInfoTopicSubscriptionArn = null;
