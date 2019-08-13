d8b55b8c2fe01a004e5fab2c991a733c9669fde7
harshita_witcurve <harshita@witcurve.com>
2019-08-13 17:46:59 +0530

    Test push notification : temp

--- src/main/java/com/witcurve/service/EventService.java
+++ src/main/java/com/witcurve/service/EventService.java
@@ -14,6 +14,8 @@ import java.util.List;
 
 public interface EventService {
 
+    String testMessage = "test";
+
     List<EventDTO> saveOrUpdate(List<EventDTO> eventDTOs, Long schoolInfoId) throws WitcurveException, UnsupportedEncodingException;
 
     EventDTO getEventById(Long eventId) throws WitcurveException;
