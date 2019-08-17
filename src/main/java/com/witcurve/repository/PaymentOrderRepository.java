fd389cb1eaa646d065827be9effa31177baa208f
Vishal Gidwani <vishal.gidwani@beehyv.com>
2019-08-17 10:57:15 +0530

    offline payment changes

--- src/main/java/com/witcurve/repository/PaymentOrderRepository.java
+++ src/main/java/com/witcurve/repository/PaymentOrderRepository.java
@@ -3,6 +3,7 @@ package com.witcurve.repository;
 import com.witcurve.domain.PaymentOrder;
 import com.witcurve.domain.enumeration.TransactionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,4 +12,7 @@ public interface PaymentOrderRepository extends JpaRepository<PaymentOrder, Long
     PaymentOrder findByOrderId(String orderId);
 
     List<PaymentOrder> findByTransactionStatus(TransactionStatus transactionStatus);
+
+    @Query("select o from PaymentOrder o where o.paymentGateway = 'OFFLINE' and o.payout = false")
+    List<PaymentOrder> findOfflineOrdersWithoutPayout();
 }
--- src/main/java/com/witcurve/web/rest/PaymentResource.java
+++ src/main/java/com/witcurve/web/rest/PaymentResource.java
@@ -1,8 +1,6 @@
 package com.witcurve.web.rest;
 
 import com.codahale.metrics.annotation.Timed;
-import com.witcurve.domain.PaymentOrder;
-import com.witcurve.domain.enumeration.PaymentGateway;
 import com.witcurve.domain.enumeration.SubscriptionPackage;
 import com.witcurve.domain.enumeration.TransactionMode;
 import com.witcurve.service.PaymentService;
@@ -16,6 +14,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class PaymentResource {
@@ -25,12 +25,15 @@ public class PaymentResource {
     @Autowired
     private PaymentService paymentService;
 
-    @PostMapping("/payment/self/create-order")
+    @PostMapping("/payment/offline/create-order")
     @Timed
-    public ResponseEntity<PaymentOrderDTO> createSelfOrder(@RequestParam Long studentId,
-                                                           @RequestParam TransactionMode transactionMode,
-                                                           @RequestParam SubscriptionPackage subscriptionPackage) throws WitcurveException {
-        return ResponseEntity.ok().body(paymentService.createPaymentOrder(studentId, subscriptionPackage, PaymentGateway.SELF, transactionMode));
+    public ResponseEntity<PaymentOrderDTO> createOfflineOrder(@RequestBody PaymentOrderDTO paymentOrderDTO) throws WitcurveException {
+        log.debug("Rest API for creating offline order");
+        if (paymentOrderDTO.getTransactionMode() != TransactionMode.CASH && paymentOrderDTO.getTransactionMode() != TransactionMode.IMPS) {
+            throw new WitcurveException("Only CASH and IMPS transaction mode is allowed in offline orders");
+        }
+
+        return ResponseEntity.ok().body(paymentService.createPaymentOrder(paymentOrderDTO));
     }
 
     @PostMapping("/payment/paytm/create-order")
@@ -38,6 +41,7 @@ public class PaymentResource {
     public ResponseEntity<PaytmRequestDTO> createPaytmOrder(@RequestParam Long studentId,
                                                             @RequestParam(required = false) String mobileNumber,
                                                             @RequestParam SubscriptionPackage subscriptionPackage) throws WitcurveException {
+        log.debug("Rest API for creating paytm order");
         return ResponseEntity.ok().body(paymentService.createPaytmOrder(studentId, mobileNumber, subscriptionPackage));
     }
 
@@ -48,5 +52,29 @@ public class PaymentResource {
                                                      @RequestParam(required = false, defaultValue = "false") Boolean updateSubscription) throws WitcurveException {
         log.debug("Rest API for verification order status {}", orderId);
         return ResponseEntity.ok().body(paymentService.isTransactionComplete(orderId, updateSubscription));
+    }
+
+    @PatchMapping("/payment/update-transaction-id")
+    @Timed
+    public ResponseEntity<PaymentOrderDTO> updateTransactionId(@RequestParam String orderId,
+                                                               @RequestParam String transactionId) throws WitcurveException {
+        log.debug("Rest API for updating transaction id of payment order");
+        return ResponseEntity.ok().body(paymentService.updateTransactionId(orderId, transactionId));
+
+    }
+
+    @PatchMapping("/payment/update-payout")
+    @Timed
+    public ResponseEntity<PaymentOrderDTO> updatePayout(@RequestParam String orderId) throws WitcurveException {
+        log.debug("Rest API for updating payout status of payment order");
+        return ResponseEntity.ok().body(paymentService.updatePayout(orderId));
+    }
+
+
+    @GetMapping("/payment/offline/pending-payout")
+    @Timed
+    public ResponseEntity<List<PaymentOrderDTO>> getOfflineOrdersWithoutPayouts() throws WitcurveException {
+        log.debug("Rest API for getting offline orders which have pending payout");
+        return ResponseEntity.ok().body(paymentService.getOfflineOrdersWithoutPayouts());
     }
 }
