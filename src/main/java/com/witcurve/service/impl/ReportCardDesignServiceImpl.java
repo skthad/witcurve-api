8dc488027b3e9ae46d67c3ec3f91a9f017588228
srujan_kumar92 <srujanthegoldenboy@gmail.com>
2019-09-24 17:13:47 +0530

    added order to all scholastic entries

--- src/main/java/com/witcurve/service/impl/ReportCardDesignServiceImpl.java
+++ src/main/java/com/witcurve/service/impl/ReportCardDesignServiceImpl.java
@@ -48,11 +48,6 @@ public class ReportCardDesignServiceImpl implements ReportCardDesignService {
         log.debug("Request to save ReportCardDesigns : {} for exam with id : {} for grade : {}", reportCardDesignDTOS, examId, grade);
         validAndFormatReportCardDesigns(reportCardDesignDTOS, examId, grade);
         List<ReportCardDesign> reportCardDesigns = reportCardDesignMapper.toEntity(reportCardDesignDTOS);
-//        for(ReportCardDesign reportCardDesign : reportCardDesigns) {
-////            if(reportCardDesign.getFieldType().equals(ReportFieldType.ATTRIBUTES) {
-////
-////            }
-////        }
         reportCardDesigns = reportCardDesignRepository.saveAll(reportCardDesigns);
         validTotalReportCardRecords(examId, grade);
         return reportCardDesignMapper.toDto(reportCardDesigns);
@@ -84,20 +79,19 @@ public class ReportCardDesignServiceImpl implements ReportCardDesignService {
     public void deleteReportCardDesign(List<Long> ids) {
         log.debug("Request to delete report card design with ids : {}");
         List<ReportCardDesign> reportCardDesigns = reportCardDesignRepository.findAllById(ids);
-//        for(ReportCardDesign reportCardDesign : reportCardDesigns) {
-//            if(!reportCardDesign.getFieldType().equals(ReportFieldType.MANUAL_ENTRY)) {
-//                throw new WitcurveException("You can only delete manual entry report card designs");
-//            }
-//        }
+        for(ReportCardDesign reportCardDesign : reportCardDesigns) {
+            if(!reportCardDesign.getFieldType().equals(ReportFieldType.MANUAL_ENTRY)) {
+                throw new WitcurveException("You can only delete manual entry report card designs");
+            }
+        }
         studentMarksRepository.deleteStudentMarksByRcdIds(ids);
         reportCardDesignRepository.deleteByIds(ids);
     }
 
     private void validAndFormatReportCardDesigns(List<ReportCardDesignDTO> reportCardDesignDTOS, Long examId, Grade grade) {
         Boolean mainRecordExists = false, totalRecordExists = false,
-            remarksRecordExists =false, attendanceRecordExists = false,
-            periodicTestRecordExists=false, nonScholasticRecordExists=false,
-            attributeRecordExists = false;
+            remarksRecordExists =false, periodicTestRecordExists=false,
+            nonScholasticRecordExists=false, attributeRecordExists = false;
         List<ReportCardDesign> existingReportCardDesigns = null;
         Optional<Exam> exam = examRepository.findById(examId);
         if(!exam.isPresent()) {
@@ -157,6 +151,9 @@ public class ReportCardDesignServiceImpl implements ReportCardDesignService {
                         }
                     }
                     if(fieldType.equals(ReportFieldType.MAIN)) {
+                        if(reportCardDesignDTO.getOrder() == null) {
+                            throw new WitcurveException("Main field needs have order");
+                        }
                         reportCardDesignDTO.setName("Exam");
                         reportCardDesignDTO.setShortForm("Exam");
                         mainRecordExists = true;
@@ -175,6 +172,9 @@ public class ReportCardDesignServiceImpl implements ReportCardDesignService {
                         }
                         attributeRecordExists = true;
                     } else {
+                        if(reportCardDesignDTO.getOrder() == null) {
+                            throw new WitcurveException("Periodic Test field needs have order");
+                        }
                         if(reportCardDesignDTO.getSelectedPeriodicTests() == null || reportCardDesignDTO.getSelectedPeriodicTests().size() == 0) {
                             throw new WitcurveException("Periodic Test field requires list of binding id values");
                         }
@@ -201,10 +201,6 @@ public class ReportCardDesignServiceImpl implements ReportCardDesignService {
                         if(remarksRecordExists) {
                             throw new WitcurveException("There should be only one remarks field type record for this model");
                         }
-                    } else {
-                        if(attendanceRecordExists) {
-                            throw new WitcurveException("There should be only one attendance field type record for this model");
-                        }
                     }
                     existingReportCardDesigns = reportCardDesignRepository.findByFieldTypeAndExamAndGrade(fieldType, examId, grade);
                     if(existingReportCardDesigns.size()==0) {
@@ -229,8 +225,6 @@ public class ReportCardDesignServiceImpl implements ReportCardDesignService {
                     reportCardDesignDTO.setMarks(null);
                     if(fieldType.equals(ReportFieldType.REMARKS)) {
                         remarksRecordExists = true;
-                    } else {
-                        attendanceRecordExists = true;
                     }
                     break;
                 case MANUAL_ENTRY:
@@ -251,8 +245,6 @@ public class ReportCardDesignServiceImpl implements ReportCardDesignService {
                     }
                     reportCardDesignDTO.setSelected(null);
                     break;
-
-
             }
         }
 
