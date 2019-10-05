7e48c88db963b5bc996442886bab619c53c7d18a
deven1197 <deven1197@gmail.com>
2019-10-05 22:25:56 +0530

    student report added

---
+++ src/main/java/com/witcurve/domain/StudentReport.java
@@ -0,0 +1,87 @@
+package com.witcurve.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "student_report")
+public class StudentReport extends AbstractAuditingEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "reportCard_id", nullable = false)
+    private ReportCard reportCard;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+    @NotNull
+    @OneToOne
+    @JoinColumn(name = "attachment_id", nullable = false)
+    private Attachment attachment;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ReportCard getReportCard() {
+        return reportCard;
+    }
+
+    public void setReportCard(ReportCard reportCard) {
+        this.reportCard = reportCard;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Attachment getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(Attachment attachment) {
+        this.attachment = attachment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof StudentReport)) return false;
+        StudentReport that = (StudentReport) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "StudentReport{" +
+            "id=" + id +
+            ", reportCard=" + reportCard +
+            ", student=" + student +
+            ", attachment=" + attachment +
+            '}';
+    }
+}
---
+++ src/main/java/com/witcurve/service/impl/StudentReportServiceImpl.java
@@ -0,0 +1,76 @@
+package com.witcurve.service.impl;
+
+import com.witcurve.domain.StudentReport;
+import com.witcurve.repository.StudentReportRepository;
+import com.witcurve.service.AttachmentService;
+import com.witcurve.service.StudentReportService;
+import com.witcurve.service.dto.StudentReportDTO;
+import com.witcurve.service.mapper.StudentReportMapper;
+import com.witcurve.web.rest.errors.WitcurveException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class StudentReportServiceImpl implements StudentReportService {
+
+    private final Logger log = LoggerFactory.getLogger(StudentReportServiceImpl.class);
+
+    @Autowired
+    StudentReportRepository studentReportRepository;
+
+    @Autowired
+    StudentReportMapper studentReportMapper;
+
+    @Autowired
+    AttachmentService attachmentService;
+
+    @Override
+    public StudentReportDTO saveOrUpdateStudentReport(StudentReportDTO studentReportDTO) {
+        log.debug("Request to save or update studentReport: {}", studentReportDTO);
+        StudentReport studentReport = studentReportMapper.toEntity(studentReportDTO);
+        studentReport = studentReportRepository.save(studentReport);
+        return studentReportMapper.toDto(studentReport);
+    }
+
+    @Override
+    public StudentReportDTO getStudentReportById(Long studentReportId) {
+        log.debug("Request to get studentReport with id : {}", studentReportId);
+        Optional<StudentReport> studentReport = studentReportRepository.findById(studentReportId);
+        if (!studentReport.isPresent()) {
+            throw new WitcurveException("No StudentReport with given id " + studentReportId);
+        }
+        return studentReportMapper.toDto(studentReport.get());
+    }
+
+    @Override
+    public List<StudentReportDTO> getStudentReportByStudentId(Long studentId) {
+        log.debug("Request to get studentReport with student id : {}", studentId);
+        List<StudentReport> studentReports = studentReportRepository.findByStudentId(studentId);
+        if (studentReports.isEmpty()) {
+            throw new WitcurveException("No StudentReport with given studentId " + studentId);
+        }
+        return studentReportMapper.toDto(studentReports);
+    }
+
+    @Override
+    public void deleteStudentReportById(Long studetReportId) {
+        log.debug("Request to delete studentReport with id {}", studetReportId);
+        Optional<StudentReport> studentReport = studentReportRepository.findById(studetReportId);
+        if (!studentReport.isPresent()) {
+            throw new WitcurveException("No StudentReport with given id " + studetReportId);
+        }
+        studentReportRepository.deleteById(studetReportId);
+        attachmentService.delete(studentReport.get().getAttachment().getId());
+    }
+}
+
+
+
+
