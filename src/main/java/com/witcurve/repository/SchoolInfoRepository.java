afaf8b1b089f1c1455f1048b42d2d9d9c6090391
harshita_witcurve <harshita@witcurve.com>
2019-11-06 13:02:45 +0530

    query refectored

--- src/main/java/com/witcurve/repository/SchoolInfoRepository.java
+++ src/main/java/com/witcurve/repository/SchoolInfoRepository.java
@@ -11,13 +11,13 @@ import java.util.List;
 @Repository
 public interface SchoolInfoRepository extends JpaRepository<SchoolInfo, Long> {
 
-    @Query("select si from SchoolInfo si left join fetch si.school s left join fetch s.institute i order by s.primaryBranch desc, s.name asc, si.primaryBoard desc, si.board, i.name ")
+    @Query("select si from SchoolInfo si left join fetch si.school s left join fetch s.institute i order by  i.name, s.primaryBranch desc, s.name asc, si.primaryBoard desc, si.board ")
     List<SchoolInfo> findAll(Long instituteId);
 
-    @Query("select si from SchoolInfo si left join fetch si.school s left join fetch s.institute i where si.school.institute.id = (select school.institute.id from SchoolInfo where id = ?1) order by s.primaryBranch desc, s.name asc, si.primaryBoard desc, si.board ,i.name ")
+    @Query("select si from SchoolInfo si left join fetch si.school s left join fetch s.institute i where si.school.institute.id = (select school.institute.id from SchoolInfo where id = ?1) order by i.name, s.primaryBranch desc, s.name asc, si.primaryBoard desc, si.board ")
     List<SchoolInfo> findAllForInstituteManager(Long schoolInfoId);
 
-    @Query("select si from SchoolInfo si left join fetch si.school s left join fetch s.institute i where si.school.id = (select school.id from SchoolInfo where id = ?1) order by si.primaryBoard desc, i.name, s.name, si.board ")
+    @Query("select si from SchoolInfo si left join fetch si.school s left join fetch s.institute i where si.school.id = (select school.id from SchoolInfo where id = ?1) order by si.primaryBoard desc, si.board ")
     List<SchoolInfo> findAllForSchoolAdmin(Long schoolInfoId);
 
     @Query("select si from SchoolInfo si order by si.school.institute.name asc, si.school.primaryBranch desc, si.school.name asc, si.primaryBoard desc, si.board ")
