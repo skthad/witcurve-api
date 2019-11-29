88baa0c792aa79be337f6cbbe21ce9d7f6731196
harshita_witcurve <harshita@witcurve.com>
2019-11-29 15:48:26 +0530

    temp

--- src/main/java/com/witcurve/repository/SessionFeeStructureRepository.java
+++ src/main/java/com/witcurve/repository/SessionFeeStructureRepository.java
@@ -21,6 +21,6 @@ public interface SessionFeeStructureRepository extends JpaRepository<SessionFeeS
     @Query("select sfs from SessionFeeStructure sfs where sfs.grade = ?1 and sfs.session.schoolInfo.id = ?2")
     List<SessionFeeStructure> findByGradeAndSchoolInfoId(Grade grade, Long schoolInfoId);
 
-    /*@Query("Select sfs from SessionFeeStructure sfs where sfs.grade in (select ss.standard.grade from StudentStandard ss where ss.student.id = ?1 and ss.active = true) and sfs.session.id = ?2")
-    List<SessionFeeStructure> findByStudentIdAndSessionId(Long studentId, Long sessionId);*/
+    @Query("Select sfs from SessionFeeStructure sfs where sfs.grade in (select ss.standard.grade from StudentStandard ss where ss.student.id = ?1 and ss.active = true) and sfs.session.id = ?2")
+    List<SessionFeeStructure> findByStudentIdAndSessionId(Long studentId, Long sessionId);
 }
