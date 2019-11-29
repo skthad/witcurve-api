756ea1d7f0c738ac31a507f0f2e2b0f8c2372b06
harshita_witcurve <harshita@witcurve.com>
2019-11-29 16:33:40 +0530

    query changed

--- src/main/java/com/witcurve/repository/FeePaymentRecordRepository.java
+++ src/main/java/com/witcurve/repository/FeePaymentRecordRepository.java
@@ -13,6 +13,6 @@ public interface FeePaymentRecordRepository extends JpaRepository<FeePaymentReco
     @Query("select fpr from FeePaymentRecord fpr where fpr.studentFeeStructure.student.id = ?1 and  fpr.studentFeeStructure.session.id = ?2 order by fpr.createdDate")
     List<FeePaymentRecord> getByStudentAndSessionId(Long studentId, Long sessionId);
 
-    @Query("select fpr from FeePaymentRecord fpr join StudentStandard ss on ss.student.id = fpr.studentFeeStructure.student.id where ss.standard.id = ?1 and fpr.studentFeeStructure.session.id = ?2 order by ss.rollNo, fpr.createdDate")
+    @Query("select fpr from FeePaymentRecord fpr join StudentStandard ss on ss.student.id = fpr.studentFeeStructure.student.id where ss.standard.id = ?1 and ss.active = true and fpr.studentFeeStructure.session.id = ?2 order by ss.rollNo, fpr.createdDate")
     List<FeePaymentRecord> getByStandardAndSessionId(Long standardId, Long sessionId);
 }
